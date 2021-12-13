c1f50676a1f6319c083dc499f5f9ef06340ba19c
dgallardo@cobiscorp.com <smFFwNrYQ4>
2021-12-13 11:59:21 -0500

    Se agregan metodos para eliminacion de lineas

--- untitled/src/com/company/Functions.java
+++ untitled/src/com/company/Functions.java
@@ -1,4 +1,61 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Functions {
+
+    private void reescribirArchivo(List<String> lista, String archivo, String ruta){
+
+        String root = ruta+archivo;
+        File f = new File(root);
+        try{
+            FileWriter fw = new FileWriter(f);
+            BufferedWriter escritura = new BufferedWriter(fw);
+            for (String linea:lista) {
+                escritura.write(linea);
+                escritura.newLine();
+
+            }
+
+            escritura.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void eliminarVariable(String var, String ruta, String archivo){
+        int numEliminado = 0;
+        List<String> lista = new ArrayList<>();
+        String linea = "";
+
+
+        File doc = new File(ruta+archivo);
+        try {
+            Scanner obj = new Scanner(doc);
+
+            while (obj.hasNextLine()){
+                linea = obj.nextLine();
+                if (!linea.contains(var)){
+                    lista.add(linea);
+                }
+            }
+            reescribirArchivo(lista, archivo, ruta);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+
+
 }
--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("A");
+        String ROOT = "C:/Users/dgallardo/Desktop/spsql/";
+        String variable="@w_sp_name";
+        String archivo = "asientot.sp";
+
+        Functions f = new Functions();
+
+        f.eliminarVariable(variable, ROOT, archivo);
+
     }
 }
