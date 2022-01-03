f02215da1460b1cf8597b344f27f3258637faf3f
dgallardo@cobiscorp.com <smFFwNrYQ4>
2022-01-03 14:30:38 -0500

    Correcciones

--- untitled/src/com/company/Main2.java
+++ untitled/src/com/company/Main2.java
@@ -1,13 +1,22 @@
 package com.company;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main2 {
     public static void main(String[] args) {
 
-        String ruta = "C:\\Users\\dgallardo\\Desktop\\spsql\\";
+/*        String ruta = "C:\\Users\\dgallardo\\Desktop\\spsql\\";
         String archivo = "asientot.sp";
         String variable = "@i_mayorizado";
 
-        Functions.eliminarVariable(variable, ruta, archivo);
+        Functions.eliminarVariable(variable, ruta, archivo);*/
+
+
+        Date date = new Date();
+        System.out.println(date);
+
 
     }
 }
